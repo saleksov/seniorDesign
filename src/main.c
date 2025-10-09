6c35e87e2a7566e1cac18db9b700e23266504db0
Spasko Aleksov <saleksov@purdue.edu>
2025-10-09 18:42:12 -0400

    Spasko Ahrp

--- src/main.c
+++ src/main.c
@@ -1,101 +1,62 @@
-// /*
-// WORK IN PROGRESS
-// / TBD
-
-// 0. Relearn micro takes input and output :(
-
-// 1. Create a send_byte(BYTE) function that sets ODR of GPIO MCU pins 
-// to send a byte to a shift register or the Synth Chip, depending on hardware
-// this needs to be serial or parallel -> Depending on SYNTH CHIP
-
-// 2. Create a send_2synth(BUTE, BYTE, BYTE) function that uses send_byte to send 3 BYTES
-// to the Synth chip to play a coresponding note
-
-// 3. Create a genSound(BOOL_playNewNote, ifTrue BYTE_whatNote) function that maintains the 
-// current notes being played plus adds new notes to be played specified in parametars
-
-//     data structure maintaned by genSound: -> Linked list of currently played notes
-//     allocate and free memory as needed, one note-> 1 link,
-
-// 4. Within the Foor Loop of 12 strings, add 2 for the buttons on screen for change of octave 
-// to avoid need of interrupts
-
-// 5. Add interrupt for configuring/ setting Metronome, Mode and Looper function
-
-// 6. LCD Code ???
-
 // // DO NOT TOUCH vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 // PA13 PA14
 // PC14 PC15
-// In particular, you should not change PA13, PA14, PC14, or PC15 since these 
+// In particular, you should not change PA13, PA14, PC14, or PC15 since these
 // are used for an External Oscillator, Debugging, and Programming interfaces.
 // // DO NOT TOUCH ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-
-
-// MUST READ
-
-// CURRENT ITERATION PB 0-3 ARE INPUTS TO BUTTONS
-                  // PB 4-7 ARE OUTPUTS TO LEDS
-// SETN IS USED FOR READIBUILITY -> CHANGE TO HARDCODING FOR SPEED LATER
-
-// INPUTS CURRENTLY SET TO PULL-DOWN -> Input is HIGH when button pressed
-// WHEN STRINGS ARE ADDED THIS NEEDS TO BE SET TO PULL-UP AS LDR WILL PULL LOW
-// AND GENERATE AN INPUT 
-
 
 #include "stm32f0xx.h"
 #include <stdint.h>
 
-#define STRING0  0
-#define STRING1  1
-#define STRING2  2
-#define STRING3  3
-#define STRING4  4
-#define STRING5  5
-#define STRING6  6
-#define STRING7  7
-#define STRING8  8
-#define STRING9  9
-#define STRING10 10
-#define STRING11 11
+uint8_t buffer0 = 0;
+uint8_t buffer1 = 0;
+uint8_t buffer2 = 0;
+uint8_t buffer3 = 0;
+uint8_t buffer4 = 0;
+uint8_t buffer5 = 0;
+uint8_t buffer6 = 0;
+uint8_t buffer7 = 0;
 
-uint8_t buffer0;
-uint8_t buffer1;
-uint8_t buffer2;
-uint8_t buffer3;
+uint8_t octave = 3;
+uint8_t bufferOctave0 = 0;
+uint8_t bufferOctave1 = 0;
 
-typedef struct ActiveNotes {
-    int SomeData;
-    struct ActiveNotes *next;
-    struct ActiveNotes *back;
-} ActiveNotes;
+uint8_t note0 = 60;
+uint8_t note1 = 62;
+uint8_t note2 = 64;
+uint8_t note3 = 65;
+uint8_t note4 = 67;
+uint8_t note5 = 69;
+uint8_t note6 = 71;
+uint8_t note7 = 72;
 
-void initb();
+void inita();
+void initc();
 void strings(void);
+void buttons(void);
 void setup_serial(void);
-void sendMIDI(uint8_t  channel, uint8_t  note, uint8_t  velocity);
+void sendMIDI(uint8_t channel, uint8_t note, uint8_t velocity);
 
 void internal_clock(void);
 
-int main(void) {
-    
+int main(void)
+{
+
   internal_clock();
-  initb();
+  inita();
+  initc();
   setup_serial();
 
-  while(1) 
+  while (1)
   {
     strings();
+    buttons();
   }
-    
-    return 0;
+
+  return 0;
 }
 
-// inita() // NEVER TOUCH // PA13 PA14
-// initc() // NEVER TOUCH // PC14 PC15
-
-void initb() 
+void inita()
 
 {
   // MODER
@@ -105,13 +66,9 @@ void initb()
   // 11: Analog mode
 
   // Enable clock for GPIOB
-  RCC -> AHBENR |= RCC_AHBENR_GPIOBEN;
+  RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 
-  // SET 0-3 to INPUT and RESET 4-7
-  GPIOB -> MODER &= 0x00000000;
-  
-  // set 8-11 to output for Demo
-  GPIOB -> MODER |= GPIO_MODER_MODER8_0 | GPIO_MODER_MODER9_0 | GPIO_MODER_MODER10_0 | GPIO_MODER_MODER11_0;
+  GPIOA->MODER &= 0xFFFF0000;
 
   // PUPDR
   // 00: No pull-up, pull-down
@@ -119,87 +76,168 @@ void initb()
   // 10: Pull-down
   // 11: Reserved
 
-  // CURRENTLY SET TO PULL-DOWN FOR BUTTONS
-  // WHEN STRINGS REPLACE BUTTONS SET TO PULL-UP
-  GPIOB -> PUPDR &= 0xFFFFFF55;
+  // SET TO PULL-UP
+  GPIOA->PUPDR &= 0xFFFF5555;
 }
 
-// Strings
-// Example of 1 strign what i should be
-// if true -> Trigger note generation
-// Have logic in note generation for if true -> detect 0 before allowed to be true again.
+void initc()
+{
+  RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+  GPIOC->MODER &= 0xFFFFFF0F;
+  GPIOC->PUPDR &= 0xFFFFFF5F;
+}
 
-// Create linked list struct for playing notes
-
-void strings(void) 
+void strings(void)
 {
   // String 0 - Output is B11
-  if ((GPIOB -> IDR) & (1 << STRING0))  buffer0 = 0;             
-  else if (buffer0 == 0)                {sendMIDI(0,60,100); buffer0 = 1;}
-  if ((GPIOB -> IDR) & (1 << STRING1))  buffer1 = 0;             
-  else if (buffer1 == 0)                {sendMIDI(0,63,100); buffer1 = 1;}
-  if ((GPIOB -> IDR) & (1 << STRING2))  buffer2 = 0;             
-  else if (buffer2 == 0)                {sendMIDI(0,64,100); buffer2 = 1;}
-  if ((GPIOB -> IDR) & (1 << STRING3))  buffer3 = 0;             
-  else if (buffer3 == 0)                {sendMIDI(0,67,100); buffer3 = 1;}
+  if ((GPIOA->IDR) & (1 << 0))
+    buffer0 = 0;
+  else if (buffer0 == 0)
+  {
+    sendMIDI(0, note0, 100);
+    buffer0 = 1;
+  }
+
+  if ((GPIOA->IDR) & (1 << 1))
+    buffer1 = 0;
+  else if (buffer1 == 0)
+  {
+    sendMIDI(0, note1, 100);
+    buffer1 = 1;
+  }
+
+  if ((GPIOA->IDR) & (1 << 2))
+    buffer2 = 0;
+  else if (buffer2 == 0)
+  {
+    sendMIDI(0, note2, 100);
+    buffer2 = 1;
+  }
+
+  if ((GPIOA->IDR) & (1 << 3))
+    buffer3 = 0;
+  else if (buffer3 == 0)
+  {
+    sendMIDI(0, note3, 100);
+    buffer3 = 1;
+  }
+
+  if ((GPIOA->IDR) & (1 << 4))
+    buffer4 = 0;
+  else if (buffer4 == 0)
+  {
+    sendMIDI(0, note4, 100);
+    buffer4 = 1;
+  }
+
+  if ((GPIOA->IDR) & (1 << 5))
+    buffer5 = 0;
+  else if (buffer5 == 0)
+  {
+    sendMIDI(0, note5, 100);
+    buffer5 = 1;
+  }
+
+  if ((GPIOA->IDR) & (1 << 6))
+    buffer6 = 0;
+  else if (buffer6 == 0)
+  {
+    sendMIDI(0, note6, 100);
+    buffer6 = 1;
+  }
+
+  if ((GPIOA->IDR) & (1 << 7))
+    buffer7 = 0;
+  else if (buffer7 == 0)
+  {
+    sendMIDI(0, note7, 100);
+    buffer7 = 1;
+  }
 }
 
-void sendMIDI(uint8_t  channel, uint8_t  note, uint8_t  velocity)
+void buttons()
 {
-    while (!(USART5->ISR & USART_ISR_TXE));  // Wait until TX buffer empty
-    USART5->TDR = 0x90 | (channel & 0x0F); 
-    while (!(USART5->ISR & USART_ISR_TXE));  // Wait until TX buffer empty
-    USART5->TDR = note; 
-    while (!(USART5->ISR & USART_ISR_TXE));  // Wait until TX buffer empty
-    USART5->TDR = velocity; 
+  if ((GPIOC->IDR) & (1 << 2))
+    bufferOctave0 = 0;
+  else if (bufferOctave0 == 0)
+  {
+    bufferOctave0 = 1;
+    if (octave < 7)
+    {
+      octave++;
+      note0 = (octave + 2) * 12;
+      note1 = (octave + 2) * 12 + 2;
+      note2 = (octave + 2) * 12 + 4;
+      note3 = (octave + 2) * 12 + 5;
+      note4 = (octave + 2) * 12 + 7;
+      note5 = (octave + 2) * 12 + 9;
+      note6 = (octave + 2) * 12 + 11;
+      note7 = (octave + 2) * 12 + 12;
+    }
+  }
+  if ((GPIOC->IDR) & (1 << 3))
+    bufferOctave1 = 0;
+  else if (bufferOctave1 == 0)
+  {
+    bufferOctave1 = 1;
+    if (octave > 0)
+    {
+      octave--;
+      note0 = (octave + 2) * 12;
+      note1 = (octave + 2) * 12 + 2;
+      note2 = (octave + 2) * 12 + 4;
+      note3 = (octave + 2) * 12 + 5;
+      note4 = (octave + 2) * 12 + 6;
+      note5 = (octave + 2) * 12 + 7;
+      note6 = (octave + 2) * 12 + 9;
+      note7 = (octave + 2) * 12 + 11;
+    }
+  }
+}
+
+void sendMIDI(uint8_t channel, uint8_t note, uint8_t velocity)
+{
+  while (!(USART5->ISR & USART_ISR_TXE))
+    ; // Wait until TX buffer empty
+  USART5->TDR = 0x90 | (channel & 0x0F);
+  while (!(USART5->ISR & USART_ISR_TXE))
+    ; // Wait until TX buffer empty
+  USART5->TDR = note;
+  while (!(USART5->ISR & USART_ISR_TXE))
+    ; // Wait until TX buffer empty
+  USART5->TDR = velocity;
 }
 
 void setup_serial(void)
 {
-    RCC -> AHBENR |= RCC_AHBENR_GPIOCEN;
+  RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
 
-    GPIOC -> MODER &= ~(GPIO_MODER_MODER12);
-    GPIOC -> MODER |= GPIO_MODER_MODER12_1;
+  GPIOC->MODER &= ~(GPIO_MODER_MODER12);
+  GPIOC->MODER |= GPIO_MODER_MODER12_1;
 
-    // SET TO HIGH SPEED?
-    // GPIOC -> OSPEEDR |= GPIO_OSPEEDR_OSPEEDR12_1 | GPIO_OSPEEDR_OSPEEDR12_0;
+  GPIOC->AFR[1] &= ~GPIO_AFRH_AFRH4;
+  GPIOC->AFR[1] |= 1 << (1 + GPIO_AFRH_AFRH4_Pos);
 
-    GPIOC -> AFR[1] &= ~GPIO_AFRH_AFRH4;
-    GPIOC -> AFR[1] |= 1 << (1 + GPIO_AFRH_AFRH4_Pos);
+  RCC->APB1ENR |= RCC_APB1ENR_USART5EN;
 
-    // RCC -> AHBENR |= RCC_AHBENR_GPIODEN;     // LAB MANUAL HAS RE, DO WE NEED? RECIVER
-    // GPIOD -> MODER &= ~GPIO_MODER_MODER2;
-    // GPIOD -> MODER |= GPIO_MODER_MODER2_1;
+  USART5->CR1 &= ~USART_CR1_UE; // Dissable UE, set UE = 0
 
-    // GPIOD -> AFR[0] &= ~GPIO_AFRL_AFRL2;
-    // GPIOD -> AFR[0] |= 1 << (1 + GPIO_AFRL_AFRL2_Pos);
+  USART5->CR1 &= ~(USART_CR1_M1 | USART_CR1_M0); // Set word size of 8 bits
 
-    RCC -> APB1ENR |= RCC_APB1ENR_USART5EN;
+  USART5->CR1 &= ~USART_CR1_OVER8; // SET TO 16X OVERSAMPLING
 
-    USART5->CR1 &= ~USART_CR1_UE; // Dissable UE, set UE = 0
+  USART5->CR1 &= ~USART_CR1_PCE; // PARITY DISSABLED
 
-    USART5->CR1 &= ~(USART_CR1_M1 | USART_CR1_M0); // Set word size of 8 bits
+  USART5->CR2 &= ~(USART_CR2_STOP_0 | USART_CR2_STOP_1); // 1 STOP BIT
 
-    USART5->CR1 &= ~USART_CR1_OVER8; // SET TO 16X OVERSAMPLING
+  USART5->BRR = 1536;
 
-    USART5->CR1 &= ~USART_CR1_PCE; // PARITY DISSABLED
+  USART5->CR1 |= USART_CR1_TE;
 
-    USART5->CR2 &= ~(USART_CR2_STOP_0 | USART_CR2_STOP_1); // 1 STOP BIT
+  USART5->CR1 |= USART_CR1_UE; // Enable UE, set UE = 1
 
-    // bAUD RATE 31250 IN BRR REGISTER
-    // USART5 -> BRR = 417; THIS IS 115200 BAUD IN LAB
-    USART5 -> BRR = 1536;
-    
-    USART5->CR1 |= USART_CR1_TE;
-    // USART5->CR1 |= USART_CR1_RE; // LAB MANUAL HAS RE, DO WE NEED? rECIVER
-
-    USART5->CR1 |= USART_CR1_UE; // Enable UE, set UE = 1
-
-    while (!((USART5 -> ISR) & (USART_ISR_TEACK 
-                          //  | USART_ISR_REACK // DO WE NEED RE?
-                                                  )));
+  while (!((USART5->ISR) & (USART_ISR_TEACK
+                            //  | USART_ISR_REACK // DO WE NEED RE?
+                            )))
+    ;
 }
-
-// QUESTIONS?
-// HOW DO I SET TO 31250 BAUD?
-// DO I SET PC12 TO HIGH SPEED?
